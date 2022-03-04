bf1930437338cd127aa7bf7c438042b2e2816e3f
tanhuynh4019 <98221135+tanhuynh4019@users.noreply.github.com>
2022-03-04 23:44:23 +0700

    push

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const route = require('./routers/routes');
 
 dotenv.config();
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 // plugin
 app.use(cors());
